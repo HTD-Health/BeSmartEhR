81b973634c1499c62486b6bbed6dce95687de4a4
matbujnowicz <matbujnowicz@gmail.com>
2022-11-28 14:03:16 +0100

    Display logged user data

--- smart-app/src/App.js
+++ smart-app/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { useEffect } from "react";
 import React from "react";
+import jwt_decode from "jwt-decode";
 
 function App({ client }) {
   const [patients, setPatients] = React.useState(null);
+  const [practitioner, setPractitioner] = React.useState(null);
+  const [scope, setScope] = React.useState(null);
 
   useEffect(() => {
     getData();
+    getUserDetails();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -14,16 +18,29 @@ function App({ client }) {
     try {
       const resp = await client.request("Patient")
       setPatients(resp);
-      console.log(await client.request("/metadata"))
     } catch (e) {
       console.log(e);
     }
   };
 
+  const getUserDetails = async () => {
+    const scope = client.state.tokenResponse.scope;
+    setScope(scope);
+
+    const token = client.state.tokenResponse.id_token;
+    const decoded = jwt_decode(token)
+
+    const resp = await client.request(decoded.fhirUser)
+    setPractitioner(resp);
+    console.log(resp)
+  }
+
   return (
     <div className="App">
       <strong>BeSmartEhR</strong>
-      <div>Logged as:</div>
+      <div>Logged as: {practitioner && practitioner.name[0].family}</div>
+      <div>scope: {scope}</div>
+      <br/>
       <div >
         {patients &&
           patients.entry.map((patient) => (
