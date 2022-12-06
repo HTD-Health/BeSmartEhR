b972ae566776f59e92902e9aafa1d7992396370c
WiktorKania <wiktor.kania@htdevelopers.com>
2022-12-06 16:14:52 +0100

    display patient info

--- epic-app/src/pages/launch.js
+++ epic-app/src/pages/launch.js
@@ -1,6 +1,143 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useMemo, useState } from "react";
 import { clientId, redirectUrl, useQuery } from "../common";
+
+const examplePatient = {
+  resourceType: "Patient",
+  id: "erXuFYUfucBZaryVksYEcMg3",
+  extension: [
+    {
+      extension: [
+        {
+          valueCoding: {
+            system: "urn:oid:2.16.840.1.113883.6.238",
+            code: "2131-1",
+            display: "Other Race",
+          },
+          url: "detailed",
+        },
+        {
+          valueString: "Other",
+          url: "text",
+        },
+      ],
+      url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race",
+    },
+    {
+      extension: [
+        {
+          valueString: "Unknown",
+          url: "text",
+        },
+      ],
+      url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity",
+    },
+    {
+      valueCodeableConcept: {
+        coding: [
+          {
+            system:
+              "urn:oid:1.2.840.114350.1.13.0.1.7.10.698084.130.657370.19999000",
+            code: "female",
+            display: "female",
+          },
+        ],
+      },
+      url: "http://open.epic.com/FHIR/StructureDefinition/extension/legal-sex",
+    },
+    {
+      valueCode: "F",
+      url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex",
+    },
+  ],
+  active: true,
+  name: [
+    {
+      use: "official",
+      text: "Camila Maria Lopez",
+      family: "Lopez",
+      given: ["Camila", "Maria"],
+    },
+    {
+      use: "usual",
+      text: "Camila Maria Lopez",
+      family: "Lopez",
+      given: ["Camila", "Maria"],
+    },
+  ],
+  telecom: [
+    {
+      system: "phone",
+      value: "469-555-5555",
+      use: "home",
+    },
+    {
+      system: "phone",
+      value: "469-888-8888",
+      use: "work",
+    },
+    {
+      system: "email",
+      value: "knixontestemail@epic.com",
+    },
+  ],
+  gender: "female",
+  birthDate: "1987-09-12",
+  deceasedBoolean: false,
+  address: [
+    {
+      use: "old",
+      line: ["3268 West Johnson St.", "Apt 117"],
+      city: "GARLAND",
+      district: "DALLAS",
+      state: "TX",
+      postalCode: "75043",
+      country: "US",
+    },
+    {
+      use: "home",
+      line: ["3268 West Johnson St.", "Apt 117"],
+      city: "GARLAND",
+      district: "DALLAS",
+      state: "TX",
+      postalCode: "75043",
+      country: "US",
+      period: {
+        start: "2019-05-24",
+      },
+    },
+  ],
+  maritalStatus: {
+    text: "Married",
+  },
+  communication: [
+    {
+      language: {
+        coding: [
+          {
+            system: "urn:ietf:bcp:47",
+            code: "en",
+            display: "English",
+          },
+        ],
+        text: "English",
+      },
+      preferred: true,
+    },
+  ],
+  generalPractitioner: [
+    {
+      reference: "Practitioner/eM5CWtq15N0WJeuCet5bJlQ3",
+      type: "Practitioner",
+      display: "Family Medicine Physician, MD",
+    },
+  ],
+  managingOrganization: {
+    reference: "Organization/enRyWnSP963FYDpoks4NHOA3",
+    display: "EHS Service Area",
+  },
+};
 
 export const LaunchPage = () => {
   const query = useQuery();
@@ -98,6 +235,7 @@ export const LaunchPage = () => {
   }, [endpoints, code]);
 
   useEffect(() => {
+    return; // todo: remove
     if (!accessToken) {
       return;
     }
@@ -118,17 +256,36 @@ export const LaunchPage = () => {
 
   return (
     <div className="App">
-      {patientData && (
-        <div>
+      {examplePatient && (
+        <>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Typography variant="h6" color="inherit" component="div">
+                BeSmartEhR - Dashboard
+              </Typography>
+            </Toolbar>
+          </AppBar>
           <div>
-            <span>First Name: </span>
-            <span>{patientData.name[0].given[0]}</span>
+            <div>
+              <span>Full name: </span>
+              <span>{examplePatient.name[0].text}</span>
+            </div>
+            <div>
+              <span>Gender: </span>
+              <span>{examplePatient.gender}</span>
+            </div>
+            <div>
+              <span>Birth date: </span>
+              <span>{examplePatient.birthDate}</span>
+            </div>
+            <div>
+              <span>Address: </span>
+              <span>
+                {examplePatient.address.find((x) => x.use === "home").line[0]}
+              </span>
+            </div>
           </div>
-          <div>
-            <span>Last Name: </span>
-            <span>{patientData.name[0].family}</span>
-          </div>
-        </div>
+        </>
       )}
     </div>
   );
