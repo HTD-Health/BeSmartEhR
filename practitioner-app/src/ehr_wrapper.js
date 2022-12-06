d8b533b45816e3426f4ddcfef557f8790a72cd39
matbujnowicz <matbujnowicz@gmail.com>
2022-12-06 15:01:32 +0100

    Use env files

--- practitioner-app/src/ehr_wrapper.js
+++ practitioner-app/src/ehr_wrapper.js
@@ -3,13 +3,6 @@ import FHIR from "fhirclient";
 import { useEffect, useState } from "react";
 import AppRouter from "./app_router";
 
-// TODO: export to env file
-const fhirUrlProviderStandAlone =
-  "http://launch.smarthealthit.org/v/r4/sim/WzIsImQ2NGIzN2Y1LWQzYjUtNGMyNS1hYmU4LTIzZWJlOGY1YTA0ZSIsImYyNTZkM2JhLWJiNzAtNDYxMy1hNjMxLTgyNWQ1MDBjNTdmYSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0/fhir";
-const client_id = "be-smart-ehr"; // whatever - as smart app launcher ignores this
-const client_secret = "completeRandom"; // whatever - as smart app launcher ignores this
-const scope = "launch/provider openid profile";
-
 const EhrWrapper = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -21,10 +14,8 @@ const EhrWrapper = () => {
   const smartLaunch = async () => {
     try {
       await FHIR.oauth2.init({
-        iss: fhirUrlProviderStandAlone,
-        clientId: client_id,
-        clientSecret: client_secret,
-        scope: scope,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        scope: process.env.REACT_APP_CLIENT_SCOPE,
       });
       setLoading(false);
     } catch (e) {
@@ -47,6 +38,7 @@ const EhrWrapper = () => {
       </Container>
     );
   }
+
   return <AppRouter />;
 };
 
