cd7ebd00571dfdc584be8084e595ec33fd42c800
ada-cienciala <adrianna.cienciala@htdevelopers.com>
2022-12-08 13:59:39 +0100

    fix up launch & clean code

--- epic-app/src/pages/launch.js
+++ epic-app/src/pages/launch.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { clientId, redirectUrl } from "../common";
 
 export const LaunchPage = () => {
-  const [patients, setPatients] = useState();
   const [currentPatient, setCurrentPatient] = useState();
 
   const [client, setClient] = useState(null);
@@ -17,21 +16,12 @@ export const LaunchPage = () => {
     try {
       const client = await FHIR.oauth2.init({
         clientId: clientId,
-        // scope: "launch/patient openid profile",
+        scope: "launch openid",
         redirectUri: redirectUrl,
       });
       setClient(client);
     } catch (e) {
       setError(e);
-    }
-  };
-
-  const getData = async () => {
-    try {
-      const resp = await client.request("Patient");
-      setPatients(resp);
-    } catch (e) {
-      console.log(e);
     }
   };
 
@@ -44,7 +34,6 @@ export const LaunchPage = () => {
     if (!client) {
       return;
     }
-    getData();
     getUserDetails();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [client]);
@@ -53,14 +42,13 @@ export const LaunchPage = () => {
     return (
       <div>
         <h3>Patient EHR Launch failed</h3>
-        <p>error: {error}</p>
+        <p>error: {JSON.stringify(error)}</p>
       </div>
     );
   }
 
   return (
     <div className="App">
-      <button onClick={smartLaunch}>Sign in</button>
       {currentPatient && (
         <div>
           <div>Current Patient:</div>
@@ -72,17 +60,6 @@ export const LaunchPage = () => {
             <span>Last Name: </span>
             <span>{currentPatient.name[0].family}</span>
           </div>
-          <br />
-          {patients && (
-            <div>
-              <div>{`Patients fetched [${patients.entry.length}]:`}</div>
-              {patients.entry.map((item) => (
-                <div key={item.resource.id}>
-                  <p>{`${item.resource.resourceType}/${item.resource.id}`}</p>
-                </div>
-              ))}
-            </div>
-          )}
         </div>
       )}
     </div>
--- epic-app/src/pages/standalone.js
+++ epic-app/src/pages/standalone.js
@@ -1,33 +1,34 @@
 import FHIR from "fhirclient";
-import { useEffect, useState } from "react";
-import { clientId, fhirUrl, redirectUrl } from "../common";
+import { useEffect, useMemo, useState } from "react";
+import { clientId, fhirUrl, redirectUrl, useQuery } from "../common";
 
 export const StandalonePage = () => {
-  const [patients, setPatients] = useState();
   const [currentPatient, setCurrentPatient] = useState();
 
   const [client, setClient] = useState(null);
   const [error, setError] = useState(null);
+
+  const query = useQuery();
+  const code = useMemo(() => query.get("code"), [query]);
+
+  useEffect(() => {
+    if (!code) {
+      return;
+    }
+    smartLaunch();
+  }, [code]);
 
   const smartLaunch = async () => {
     try {
       const client = await FHIR.oauth2.init({
         iss: fhirUrl,
         clientId: clientId,
-        scope: "launch patient/*.read openid profile",
-        redirectUri: `${redirectUrl}/standalone`,
+        scope: "launch patient/*.read openid profile offline_access",
+        redirectUri: redirectUrl,
       });
       setClient(client);
     } catch (e) {
       setError(e);
-    }
-  };
-
-  const getData = async () => {
-    try {
-      const resp = await client.request("Patient");
-      setPatients(resp);
-    } catch (e) {
       console.log(e);
     }
   };
@@ -42,7 +43,6 @@ export const StandalonePage = () => {
       return;
     }
     getUserDetails();
-    getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [client]);
 
@@ -50,14 +50,14 @@ export const StandalonePage = () => {
     return (
       <div>
         <h3>Patient Standalone Launch failed</h3>
-        <p>error: {error}</p>
+        <p>error: {JSON.stringify(error)}</p>
       </div>
     );
   }
 
   return (
     <div className="App">
-      <button onClick={smartLaunch}>Sign in</button>
+      {!client && <button onClick={smartLaunch}>Sign in</button>}
       {currentPatient && (
         <div>
           <div>Current Patient:</div>
@@ -69,17 +69,6 @@ export const StandalonePage = () => {
             <span>Last Name: </span>
             <span>{currentPatient.name[0].family}</span>
           </div>
-          <br />
-          {patients && (
-            <div>
-              <div>{`Patients fetched [${patients.entry.length}]:`}</div>
-              {patients.entry.map((item) => (
-                <div key={item.resource.id}>
-                  <p>{`${item.resource.resourceType}/${item.resource.id}`}</p>
-                </div>
-              ))}
-            </div>
-          )}
         </div>
       )}
     </div>
