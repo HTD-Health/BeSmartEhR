8f4a057d96ba6932ef4d0a7cbea43718e2b9fb1a
ada-cienciala <adrianna.cienciala@htdevelopers.com>
2022-12-09 15:01:24 +0100

    Fix token issue

--- patient-app/src/standalone_wrapper.tsx
+++ patient-app/src/standalone_wrapper.tsx
@@ -1,36 +1,34 @@
 import { Container, Typography } from '@mui/material';
 import FHIR from 'fhirclient';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import App from 'app';
 
 const StandaloneWrapper = (): JSX.Element => {
+    const initialized = useRef(true); // Check if react already rendered this component
+
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<any>(null);
-    const [client, setClient] = useState<any>(null);
 
     useEffect(() => {
-        console.log('client', client);
-        if (client) return;
+        // Epic does not allow init twice (token request returns 400)
+        // React in development renders twice (StrictMode) so we need a flag
+        if (initialized.current) {
+            initialized.current = false;
+            return;
+        }
         smartLaunch();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const smartLaunch = async (): Promise<void> => {
         try {
-            const c = await FHIR.oauth2.init({
-                // iss: process.env.REACT_APP_FHIR_URL,
-                // clientId: process.env.REACT_APP_CLIENT_ID,
-                // scope: process.env.REACT_APP_CLIENT_SCOPE,
-                // redirectUri: process.env.REACT_APP_REDIRECT_URI
-                iss: 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/',
-                clientId: '9976354f-e38c-406e-9827-3b7ede3e2061',
-                scope: 'launch patient/*.read openid profile fhirUser online_access',
-                redirectUri: 'http://localhost:3000'
+            await FHIR.oauth2.init({
+                iss: process.env.REACT_APP_FHIR_URL,
+                clientId: process.env.REACT_APP_CLIENT_ID,
+                scope: process.env.REACT_APP_CLIENT_SCOPE,
+                redirectUri: process.env.REACT_APP_REDIRECT_URI
             });
             setLoading(false);
-            console.log('- init', c);
-            setClient(c);
         } catch (e: any) {
             setError(e);
         }
