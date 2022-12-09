2427005f052d88c6d33c0ed1d5217399c4ee4bc0
ada-cienciala <adrianna.cienciala@htdevelopers.com>
2022-12-09 15:21:47 +0100

    delete patient-profile route

--- patient-app/src/app_router.tsx
+++ patient-app/src/app_router.tsx
@@ -1,17 +1,15 @@
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import PatientProfile from './pages/patient_profile/patient_profile';
+import Home from './pages/home/home';
 
 export const routes = {
-    root: '/',
-    patientProfile: '/patient-profile'
+    root: '/'
 };
 
 const AppRouter = (): JSX.Element => (
     <HashRouter>
         <Routes>
-            <Route path={routes.root} element={<Navigate to={routes.patientProfile} />} />
-            <Route path={routes.patientProfile} element={<PatientProfile />} />
+            <Route path={routes.root} element={<Home />} />
         </Routes>
     </HashRouter>
 );
