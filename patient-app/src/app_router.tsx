1c64493db374b2f8287093b568976ec0527a6d07
ada-cienciala <adrianna.cienciala@htdevelopers.com>
2022-12-09 15:01:39 +0100

    bring back patientProfile route

--- patient-app/src/app_router.tsx
+++ patient-app/src/app_router.tsx
@@ -1,17 +1,17 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import PatientProfile from './pages/patient_profile/patient_profile';
 
 export const routes = {
-    root: '/'
-    // patientProfile: '/patient-profile'
+    root: '/',
+    patientProfile: '/patient-profile'
 };
 
 const AppRouter = (): JSX.Element => (
     <HashRouter>
         <Routes>
-            {/* <Route path={routes.root} element={<Navigate to={routes.patientProfile} />} /> */}
-            <Route path={routes.root} element={<PatientProfile />} />
+            <Route path={routes.root} element={<Navigate to={routes.patientProfile} />} />
+            <Route path={routes.patientProfile} element={<PatientProfile />} />
         </Routes>
     </HashRouter>
 );
