1442f095b7dbc07c04abef57d415335e9c200503
Ada Cieńciała <adrianna.cienciala@htdhealth.com>
2025-04-24 16:55:44 +0200

    install dotenv & change default port of cds-service to 3011

--- cds-service/src/config.ts
+++ cds-service/src/config.ts
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
-  port: process.env.PORT || 3010,
+  port: process.env.PORT || 3011,
   serviceName: 'HTD Health CDS Service',
   icons: {
     logo: 'https://htdhealth.com/wp-content/themes/htd/assets/favicons/light/favicon-96x96.png',
