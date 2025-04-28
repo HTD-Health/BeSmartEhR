091404635754e1b4448008062960a81b47a9bbdb
Ada Cieńciała <adrianna.cienciala@htdhealth.com>
2025-04-28 12:19:14 +0200

    test

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Default to practitioner-app if DEPLOY_APP is not set
-APP_DIR=${DEPLOY_APP:-practitioner-app}
-
-# Change to the specified app directory and start
-cd "$APP_DIR" && npm start
