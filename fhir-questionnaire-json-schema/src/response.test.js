04aebb72ce9c8504951722182f36a2bea35a19f6
Ada Cieńciała <86961468+ada-cienciala@users.noreply.github.com>
2025-05-01 10:12:48 +0200

    [practitioner-app] Heroku hosting & fhir-questionnaire-json-schema as outside deps (#25)

--- fhir-questionnaire-json-schema/src/response.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const questionnaire = require("./testQuestionnaire.json");
-const response_1 = require("./response");
-test("Test response", () => {
-    const schema = (0, response_1.toQuestionnaireResponse)(questionnaire, { "/54126-8": { "/54126-8/54134-2": ["LA6156-9", "LA10617-1"] } });
-    console.log(JSON.stringify(schema, null, 2));
-});
