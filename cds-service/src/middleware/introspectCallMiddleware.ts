a9aaa52e29d836f5be85b599f900ad044e63c9d6
Ada Cieńciała <adrianna.cienciala@htdhealth.com>
2025-06-18 16:56:29 +0200

    try diff url

--- cds-service/src/middleware/introspectCallMiddleware.ts
+++ cds-service/src/middleware/introspectCallMiddleware.ts
@@ -11,38 +11,42 @@ export const introspectCallMiddleware = async (
   const { body } = req;
   const accessToken = body?.fhirAuthorization?.access_token;
 
-  const introspectRes = await fetch(
-    `https://vendorservices.epic.com/oauth2/Introspect`,
-    {
-      // https://vendorservices.epic.com/interconnect-amcurprd-oauth/api/FHIR/R4
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({
-        token: body?.fhirAuthorization?.access_token,
-        epic_user_id_type: 'internal',
-      }),
-    }
-  );
+  try {
+    const introspectRes = await fetch(
+      `https://vendorservices.epic.com/interconnect-amcurprd-oauth/oauth2/Introspect`,
+      {
+        // https://vendorservices.epic.com/interconnect-amcurprd-oauth/api/FHIR/R4
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          token: body?.fhirAuthorization?.access_token,
+          epic_user_id_type: 'internal',
+        }),
+      }
+    );
 
-  if (!introspectRes.ok) {
-    logger.error('Introspect call failed', {
+    logger.info('Introspect response', {
       status: introspectRes.status,
       statusText: introspectRes.statusText,
     });
-    next();
-    return;
+
+    if (!introspectRes.ok) {
+      next();
+      return;
+    }
+
+    const introspectResBody = await introspectRes.json();
+    logger.info('Introspect response', {
+      body: introspectResBody,
+    });
+  } catch (error) {
+    logger.error('Error during introspection', {
+      error: error instanceof Error ? error.message : 'Unknown error',
+    });
   }
-
-  const introspectResBody = await introspectRes.json();
-
-  logger.info('Introspect response', {
-    status: introspectRes.status,
-    statusText: introspectRes.statusText,
-    body: introspectResBody,
-  });
 
   next();
 };
