963d581187f70b2ccf2997e76eb31cb826eda39f
Ada Cieńciała <adrianna.cienciala@htdhealth.com>
2025-07-22 17:25:57 +0200

    fix log

--- practitioner-app/src/launch_wrappers/ehr_wrapper.tsx
+++ practitioner-app/src/launch_wrappers/ehr_wrapper.tsx
@@ -17,7 +17,10 @@ const EhrWrapper = (): JSX.Element => {
                 scope: import.meta.env.VITE_APP_CLIENT_SCOPE,
                 redirectUri: import.meta.env.VITE_APP_REDIRECT_URI
             });
-            console.info('SMART launch from CDS Hooks', client.state.tokenResponse?.launchedFromCDS);
+            if (client.state.tokenResponse?.appContext) {
+                const appContext = JSON.parse(decodeURIComponent(client.state.tokenResponse.appContext));
+                console.info('SMART launch from CDS Hooks', appContext.launchedFromCDS);
+            }
         } catch (e: any) {
             console.error('Error during SMART launch:', e);
             if (e instanceof Error) {
